4b8c837c19b088b8c7d6d652e3fc08a9484726c6
Chloe <chloeg96@gmail.com>
2018-05-04 04:15:11 -0400

    things are training now, hope they are correct

---
+++ generate_fashions.py
@@ -0,0 +1,34 @@
+import random
+import numpy
+import theano
+import fashion_model
+
+m = fashion_model.Model(artificial_font=True)
+m.try_load()
+run_fn = m.get_run_fn()
+W = m.get_font_embeddings()
+cov = numpy.cov(W.T)
+
+def generate_font():
+    return numpy.random.multivariate_normal(mean=numpy.zeros(m.d), cov=cov)
+
+def generate_input(n_fonts=5):
+    fonts = [generate_font() for f in xrange(n_fonts)]
+    for f in xrange(n_fonts):
+        a, b = fonts[f], fonts[(f+1)%n_fonts]
+        for p in numpy.linspace(0, 1, 10):
+            print f, p
+            batch_is = numpy.zeros((m.k, m.d), dtype=theano.config.floatX)
+            batch_js = numpy.zeros((m.k,), dtype=numpy.int32)
+            for z in xrange(m.k):
+                batch_is[z] = a * (1-p) + b * p
+                batch_js[z] = z
+
+            yield batch_is, batch_js
+
+print 'generating...'
+frame = 0
+for input_i, input_j in generate_input():
+    img = fashion_model.draw_grid(run_fn(input_i, input_j))
+    img.save('font_%06d.png' % frame)
+    frame += 1
--- train_fashion_model.py
+++ train_fashion_model.py
@@ -1,34 +1,42 @@
 import random
 import numpy
 import theano
-import model
+import fashion_model
 import sys
 import functools
 import time
+# import parse_images
 from scipy.ndimage import filters
 
+# def iterate_minibatches(dataset, batch_size=512):
 def iterate_minibatches(dataset, batch_size=512):
     random.shuffle(dataset)
     for offset in xrange(0, len(dataset), batch_size):
         s = min(batch_size, len(dataset) - offset)
         batch_fonts = numpy.zeros((s,), dtype=numpy.int32)
-        batch_chars = numpy.zeros((s,), dtype=numpy.int32)
+        # batch_chars = numpy.zeros((s,), dtype=numpy.int32)
         batch_ds = numpy.zeros((s, wh), dtype=theano.config.floatX)
         for z in xrange(s):
             i, j = dataset[offset + z]
+            # i = dataset[offset + z]
             batch_fonts[z] = i
-            batch_chars[z] = j
-            m = filters.gaussian_filter(data[i][j], sigma=random.random()*1.0) # data augmentation
+            # batch_chars[z] = j
+            # m = filters.gaussian_filter(data[i][j], sigma=random.random()*1.0) # data augmentation
+            m = filters.gaussian_filter(data[i], sigma=random.random()*1.0) # data augmentation
             batch_ds[z] = m.flatten() * 1. / 255
 
-        yield s, 1.0 * offset / len(dataset), batch_fonts, batch_chars, batch_ds
+        # yield s, 1.0 * offset / len(dataset), batch_fonts, batch_chars, batch_ds
+        yield s, 1.0 * offset / len(dataset), batch_fonts, batch_ds
+
 
         
 def iterate_run(dataset, fn, tag):
     total_loss, total_reg, total_count = 0, 0, 0
-    for s, progress, input_font, input_char, output in iterate_minibatches(dataset):
+    # for s, progress, input_font, input_char, output in iterate_minibatches(dataset):
+    for s, progress, input_font, output in iterate_minibatches(dataset):
         t0 = time.time()
-        loss, reg = fn(input_font, input_char, output)
+        # loss, reg = fn(input_font, input_char, output)
+        loss, reg = fn(input_font, output)
         t = time.time() - t0
         total_loss += float(loss) * s
         total_reg += float(reg) * s
@@ -40,15 +48,18 @@ def iterate_run(dataset, fn, tag):
     return total_loss / total_count
 
 
-data = model.get_data()
+data = fashion_model.get_data()
 n, k = data.shape[0], data.shape[1]
+# n = data.shape[0]
 wh = data.shape[2] * data.shape[3]
-model = model.Model(n, k, wh)
-model.try_load()
-train_fn_w_learning_rate = model.get_train_fn()
-test_fn = model.get_test_fn()
-run_fn = model.get_run_fn()
-train_set, test_set = model.sets()
+# wh = data.shape[2] * data.shape[3]
+fashion_model = fashion_model.Model(n, k, wh)
+# model = model.Model(n, wh)
+fashion_model.try_load()
+train_fn_w_learning_rate = fashion_model.get_train_fn()
+test_fn = fashion_model.get_test_fn()
+run_fn = fashion_model.get_run_fn()
+train_set, test_set = fashion_model.sets()
 
 print 'training...'
 for learning_rate in [1.0, 0.3, 0.1, 0.03, 0.01]:
@@ -61,5 +72,5 @@ for learning_rate in [1.0, 0.3, 0.1, 0.03, 0.01]:
         if loss > last_loss and epoch > 3:
             break # decrease learning rate
         last_loss = loss
-        model.save()
+        fashion_model.save()
         epoch += 1
