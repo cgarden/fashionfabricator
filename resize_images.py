12fbc4563f6df27364a4db413a7d1f5d2edb8a9a
Chloe <chloeg96@gmail.com>
2018-05-14 01:17:08 -0400

    changes

--- resize_images.py
+++ resize_images.py
@@ -1,11 +1,11 @@
-from PIL import Image
+import Image
 from resizeimage import resizeimage
 import glob
 
 addrs = glob.glob('../img/*/*.jpg')
 
 for addr in addrs:
-	fd_img = open(addr,'r+w')
+	fd_img = open(addr,'r')
 	img = Image.open(fd_img)
 	img = resizeimage.resize_contain(img, [64, 64])
 	img.convert('L')
