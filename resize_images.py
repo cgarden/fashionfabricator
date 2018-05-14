62dc1884d0b6699124e9b52a9c70487b4890001a
Chloe <chloeg96@gmail.com>
2018-05-14 01:46:54 -0400

    changed it

--- resize_images.py
+++ resize_images.py
@@ -4,10 +4,17 @@ import glob
 
 addrs = glob.glob('../img/*/*.jpg')
 
+i=0
 for addr in addrs:
 	fd_img = open(addr,'r+b')
-	img = Image.open(fd_img)
-	img = resizeimage.resize_contain(img, [64, 64])
-	img.convert('L')
-	img.save(addr, img.format)
-	fd_img.close()
+	try:
+		img = Image.open(fd_img)
+		img = resizeimage.resize_contain(img, [64, 64])
+		img.convert('L')
+		img.save(addr, img.format)
+		fd_img.close()
+		i+=1
+		print i
+	except IOError:
+		i+=1
+		continue
